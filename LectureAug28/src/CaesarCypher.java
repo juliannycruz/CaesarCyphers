e7f74612c442d7f81cd0a8099812b9d6442df42c
julia <julia@DESKTOP-OJIGLSI>
2018-09-04 14:16:15 -0400

    Implement encoding function to caesar cypher.

--- LectureAug28/src/CaesarCypher.java
+++ LectureAug28/src/CaesarCypher.java
@@ -2,17 +2,27 @@
 public class CaesarCypher {
 
 	private int key;
-	
+
 	private final static String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	
+
 	private static String shiftedAlphabet;
-	
+
 	public CaesarCypher(int key) {
 		this.key = key;
 		String leftSubstring = alphabet.substring(0, key);
 		String rightSubstring = alphabet.substring(key);
 		shiftedAlphabet = rightSubstring + leftSubstring;
 	}
-	
-	
+
+	public String encode(String clearText) {
+		String cypherText = "";
+		for (int i = 0; i < clearText.length(); i++) {
+			if (Character.isAlphabetic(clearText.charAt(i))) {
+				cypherText += shiftedAlphabet.charAt(alphabet.indexOf(clearText.charAt(i)));
+			} else
+				cypherText += clearText.charAt(i);
+		}
+		return cypherText;
+	}
+
 }
--- LectureAug28/src/Main.java
+++ LectureAug28/src/Main.java
@@ -3,9 +3,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		double sqrt2 = Math.sqrt(2.0);
-		
-		
+		String name = "Fernando hola";
+		CaesarCypher cypher = new CaesarCypher(4);
+		name = cypher.encode(name);
+		System.out.println(name);
 		
 	}
 	
